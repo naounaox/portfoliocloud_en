4cd5aea127dc19de5d788dc02eeba99689992ca2
naoyuki <naounaox@icloud.com>
2025-02-09 23:51:41 +0900

    smartphone applied5

--- components/HomeSection.js
+++ components/HomeSection.js
@@ -34,7 +34,7 @@ export default function HomeSection() {
         height: { xs: 350, sm: 600, md: 850 }, // デバイスごとの高さ
         width: 'auto', // 横幅は自動調整
         marginTop: { xs: 0, sm: 2, md: 2 }, // 上の余白
-        marginBottom: { xs: 5, sm: 2, md: 2 }, // 下の余白
+        marginBottom: { xs: 5, sm: 2, md: 0 }, // 下の余白
         display: 'block', // 中央に配置するための設定
         marginLeft: 'auto',
         marginRight: 'auto',
@@ -44,31 +44,37 @@ export default function HomeSection() {
 
       {/* タイトル */}
       <Typography
-        variant="h2"
-        sx={{
-          fontFamily: 'Shadows Into Light, cursive',
-          animation: 'fadeIn 1s ease-in-out 1s', 
-          color: '#202020', // 文字色
-          marginTop: -15,
-          marginBottom: 2,
-        }}
-      >
-        UM Portfolio
-      </Typography>
+  variant="h2"
+  sx={{
+    fontFamily: 'Shadows Into Light, cursive',
+    animation: 'fadeIn 1s ease-in-out 1s', 
+    color: '#202020', // 文字色
+    fontSize: { xs: '4rem', sm: '5rem', md: '6rem' }, // スマホ, タブレット, PCごとのフォントサイズ
+    marginTop: { xs: '-10px', sm: '-20px', md: '-100px' }, // デバイスごとに調整
+    marginBottom: { xs: '10px', sm: '10px', md: '10px' }, // デバイスごとに調整
+    textAlign: 'center', // 画面中央に配置
+  }}
+>
+  UM Portfolio
+</Typography>
 
-      {/* キャッチコピー */}
-      <Typography
-        variant="h4"
-        sx={{
-          fontFamily: 'Kosugi Maru, sans-serif',
-          animation: 'fadeIn 1.5s ease-in-out 0.5s',
-          color: '#202020', // 文字色
-        }}
-      >
-         {showJapanese
-          ? 'わかりやすく、楽しく、心に残るデザインを！'
-          : 'Clear, Fun, and Memorable Designs!'}
-      </Typography>
+{/* キャッチコピー */}
+<Typography
+  variant="h4"
+  sx={{
+    fontFamily: 'Kosugi Maru, sans-serif',
+    animation: 'fadeIn 1.5s ease-in-out 0.5s',
+    color: '#202020', // 文字色
+    fontSize: { xs: '1.5rem', sm: '2.0rem', md: '2.5rem' }, // デバイスごとにフォントサイズを変更
+    textAlign: 'center', // 画面中央に配置
+    marginTop: { xs: '5px', sm: '10px', md: '15px' }, // 余白調整
+  }}
+>
+  {showJapanese
+    ? 'わかりやすく、楽しく、心に残るデザインを！'
+    : 'Clear, Fun, and Memorable Designs!'}
+</Typography>
+
     </Box>
   );
 }
