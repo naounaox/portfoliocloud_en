37b3d2b5bbfa4c14c29e6b2c1ad02dc3a80c95f3
naoyuki <naounaox@icloud.com>
2025-03-29 16:58:05 +0900

    favicon add

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&family=Kosugi+Maru&family=Montserrat&family=Luckiest+Guy&family=DM+Sans&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
         {/* ビューポートの設定 */}
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
